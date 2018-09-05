dcb45d5e0370ffd78147898cdff768eba525359e
UDAY BAG <uday625@Nemesis-2.local>
2018-09-05 11:08:46 -0400

    clean codes in CardList

--- src/CardList.js
+++ src/CardList.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList= ({robots})=>{
-
-    const cardComponent = robots.map((item,index)=>{
-        return (
-        <Card 
-            key={index} 
-            id={robots[index].id} 
-            name={robots[index].name} 
-            email={robots[index].email}/>
-        );
-    })
     return(
         <div>
-            {cardComponent}
+            {
+            robots.map((item,index)=>{
+                    return (
+                            <Card 
+                                key={index} 
+                                id={robots[index].id} 
+                                name={robots[index].name} 
+                                email={robots[index].email}
+                            />
+                    );
+                })
+            }
         </div>
     );
 }
