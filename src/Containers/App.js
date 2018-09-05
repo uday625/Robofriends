0fb15949532bee31a1123a993e2de79feb86fc5a
UDAY BAG <uday625@Nemesis-2.local>
2018-09-05 18:22:39 -0400

    phase1 final version

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll';
 import './App.css'
 
 class App extends Component {
@@ -29,17 +30,20 @@ class App extends Component {
 
     render(){
 
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length === 0){
+        if(robots.length === 0){
             return <h1>Loading</h1>
         }else{
             return(
                 <div className="tc">            
                     <h1 className='f1' >RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
