65d44f5992bfacd35f43d83d5200da8aaeb18e75
UDAY BAG <uday625@Nemesis-2.local>
2018-09-06 11:07:13 -0400

    adds redux logger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import { createLogger} from 'redux-logger';
 import './index.css';
 import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +10,8 @@ import {searchRobots} from './reducers';
 import 'tachyons';
 
 
-const store = createStore (searchRobots);
+const logger = createLogger();
+const store = createStore (searchRobots, applyMiddleware(logger));
 
 
 ReactDOM.render(
