ca8d1b81bb3e98d73cde6d51d197cb7e6ad0e66f
UDAY BAG <uday625@Nemesis-2.local>
2018-09-06 12:21:52 -0400

    adds async call in redux

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -6,53 +6,36 @@ import Scroll from '../Components/Scroll';
 import ErrorBoundary from '../Components/ErrorBoundary';
 import './App.css'
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
     return {
-        searchField:state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error       
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onrequestRobots: () =>  dispatch(requestRobots())
     }
 }
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state={
-            robots: []//,
-            //searchfield: ''            
-        }
-    }
 
-    componentDidMount(){
-        //console.log(this.props.store.getState());
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>{
-            return response.json();
-        })
-        .then(users =>{
-            this.setState({robots:users});
-        })
-        
+    componentDidMount(){       
+        this.props.onrequestRobots();
     }
-
-    // onSearchChange = (event)=>{
-    //     this.setState({searchfield: event.target.value})
-    // }
 
     render(){
-
-        const {robots } = this.state;
-        const {searchField, onSearchChange} = this.props
+        const {robots, searchField, onSearchChange, isPending} = this.props
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        if(robots.length === 0){
+        if(!isPending){
             return <h1>Loading</h1>
         }else{
             return(
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,38 @@
-import { CHANGE_SEARCH_FIELD }  from './constant';
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+    } from './constant';
 
-const intialState = { 
+const intialStateSearch = { 
     searchField: ''
 }
 
-export const searchRobots = (state=intialState, action={}) =>{
+export const searchRobots = (state=intialStateSearch, action={}) =>{
     switch (action.type){
 
         case CHANGE_SEARCH_FIELD: 
             return Object.assign({}, state, {searchField: action.payload});
         default: 
             return state;
+    }
+}
+
+const initialStateRobots = {
+    robots: [],
+    isPending: true
+  }
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+    switch (action.type) {
+      case REQUEST_ROBOTS_PENDING:
+        return Object.assign({}, state, {isPending: true})
+      case REQUEST_ROBOTS_SUCCESS:
+        return Object.assign({}, state, {robots: action.payload, isPending: false})
+      case REQUEST_ROBOTS_FAILED:
+        return Object.assign({}, state, {error: action.payload})
+      default:
+        return state
     }
 }
